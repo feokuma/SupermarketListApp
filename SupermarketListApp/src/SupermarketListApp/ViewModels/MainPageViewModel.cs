74903ab6207a0f61f76d2b95d6f6263b2dd52aa9
Fernando Okuma <fernando.okuma@lambda3.com.br>
2020-04-08 23:27:41 -0300

    Ajust folders and add unit test project

---
+++ SupermarketListApp/src/SupermarketListApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace SupermarketListApp.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public string MainLabel { get; private set; }
+
+        public MainPageViewModel()
+        {
+            MainLabel = "Hello World";
+        }
+    }
+}
---
+++ SupermarketListApp/test/SupermarketApp.UnitTests/ViewModel/MainPageViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using SupermarketListApp.ViewModels;
+
+namespace SupermarketApp.UnitTests.ViewModel
+{
+    public class MainPageViewModelTests
+    {
+        [Test]
+        public void Test()
+        {
+            var viewModel = new MainPageViewModel();
+
+            Assert.AreEqual("Hello World!", viewModel.MainLabel);
+        }
+    }
+}
