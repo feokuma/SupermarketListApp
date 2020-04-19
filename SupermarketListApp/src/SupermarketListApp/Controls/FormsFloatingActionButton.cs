11bb5463e5c604992ea216d122ff512ef7db6d14
Fernando Okuma <fernando.okuma@lambda3.com.br>
2020-04-19 17:47:45 -0300

    Adicionado FloatingActionButton no Android

---
+++ SupermarketListApp/src/SupermarketListApp/Controls/FormsFloatingActionButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SupermarketListApp.Controls
+{
+    public class FormsFloatingActionButton : Button
+    {
+        public FormsFloatingActionButton()
+        {
+        }
+    }
+}
--- SupermarketListApp/src/SupermarketListApp/ViewModels/ShoppingListViewModel.cs
+++ SupermarketListApp/src/SupermarketListApp/ViewModels/ShoppingListViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Windows.Input;
+using Prism.Commands;
+
 namespace SupermarketListApp.ViewModels
 {
     public class ShoppingListViewModel
     {
+        public ICommand NavigateToSearchItemCommand { get; private set; } 
         public ShoppingListViewModel()
         {
+            NavigateToSearchItemCommand = new DelegateCommand(NavigateToSearchItem);
+        }
+
+        private void NavigateToSearchItem()
+        {
+            System.Diagnostics.Debug.WriteLine("O floating button foi acionado");
         }
     }
 }
